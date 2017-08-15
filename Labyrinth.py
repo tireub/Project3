53900e3528e923daa8a258683a5913bfb01c1c20
tireub <tireub@msn.com>
2017-08-15 19:08:37 +0200

    Implemented forbidden movement if wall or window border
    Added congratulation message and implemented end game

--- Labyrinth.py
+++ Labyrinth.py
@@ -44,13 +44,9 @@ class Labyrinth:
                 # switch between the different types of sprites
                 if sprites == "w" or sprites == "g":
                     window.blit(background, (x, y))
-                    if sprites == "g":
-                        #char_pos = (x + 7, y)
-                        #char_sprite = (x_pos, y_pos)
-
-                elif sprites == "b":
+                if sprites == "b":
                     window.blit(wall, (x, y))
-                elif sprites == "r":
+                if sprites == "r":
                     window.blit(finish, (x, y))
                 x_pos += 1
 
@@ -63,17 +59,36 @@ class character:
     #def initialisation of the character
     def __init__(self, char_pos):
         self.pos = char_pos
+        self.sprite = char_sprite
         self.img = pygame.image.load("img/mcgiver.png").convert()
+        print(self.sprite)
 
     def move(self,direction):
+
+        #while receiving a direction, check if it's a wall or the border, else move
         if direction == "up":
-            self.pos = [(self.pos[0]),(self.pos[1] - 45)]
+            if self.sprite[0] > 0:
+                if level.grid[self.sprite[0] - 1][self.sprite[1]] != "b":
+                    self.pos = [(self.pos[0]),(self.pos[1] - 45)]
+                    self.sprite = [(self.sprite[0] - 1), (self.sprite[1])]
         if direction == "down":
-            self.pos = [(self.pos[0]),(self.pos[1] + 45)]
+            if self.sprite[0] < 14:
+                if level.grid[self.sprite[0] + 1][self.sprite[1]] != "b":
+                    self.pos = [(self.pos[0]), (self.pos[1] + 45)]
+                    self.sprite = [(self.sprite[0] + 1), (self.sprite[1])]
         if direction == "right":
-            self.pos[0] += 45
+            if self.sprite[1] < 14:
+                if level.grid[self.sprite[0]][self.sprite[1] + 1] != "b":
+                    self.pos = [(self.pos[0] + 45), (self.pos[1])]
+                    self.sprite = [(self.sprite[0]), (self.sprite[1] + 1)]
         if direction == "left":
-            self.pos[0] -= 45
+            if self.sprite[1] > 0:
+                if level.grid[self.sprite[0]][self.sprite[1] - 1] != "b":
+                    self.pos = [(self.pos[0] - 45), (self.pos[1])]
+                    self.sprite = [(self.sprite[0]), (self.sprite[1] - 1)]
+
+
+
 
 
 
@@ -94,6 +109,7 @@ window = pygame.display.set_mode((675,675))
 background = pygame.image.load("img/background5.jpg")
 wall = pygame.image.load("img/wall2.jpg")
 finish = pygame.image.load("img/finish.jpg")
+congrats_message = pygame.image.load("img/winner.jpg")
 
 #Create the level
 level = Labyrinth('Level1')
@@ -156,4 +172,15 @@ while loop:
     window.blit(mcgiver.img, (mcgiver.pos[0], mcgiver.pos[1]))
 
     # Screen refresh
+    pygame.display.flip()
+
+    if level.grid[mcgiver.sprite[0]][mcgiver.sprite[1]] == "r":
+        break
+
+while loop:
+    window.blit(congrats_message,(0,0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            loop = 0
+
     pygame.display.flip()
